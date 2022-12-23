df692154d253fdbcc8da0ac1496f1085f1e50fb1
An T.G <boltcarrier@naver.com>
2022-12-23 23:03:55 +0900

    객체지향 프로그래밍 실습 - 음식점에서 음식 주문하는 과정 구현

---
+++ src/main/java/org/example/order/Cooking.java
@@ -0,0 +1,11 @@
+package org.example.order;
+
+public class Cooking {
+
+    public Cook makeCook(MenuItem menuItem) {
+        Cook cook = new Cook(menuItem);
+        return cook;
+    }
+
+
+}
---
+++ src/main/java/org/example/order/Customer.java
@@ -0,0 +1,9 @@
+package org.example.order;
+
+public class Customer {
+
+    public void order(String menuName, Menu menu, Cooking cooking) {
+        MenuItem menuItem = menu.choose(menuName);
+        Cook cook = cooking.makeCook(menuItem);
+    }
+}
